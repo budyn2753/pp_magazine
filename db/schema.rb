6421d6ccd1c4a59207d1482f186b2185c52b4c3d
budyn2753 <grzelak.dariusz93@gmail.com>
2020-10-30 12:33:15 +0100

    after installation

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 ActiveRecord::Schema.define(version: 2020_10_30_102151) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "devices", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
